d36fe6f86585532185be07bc517bfcdc65e29f26
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2025-05-15 12:20:24 +0900

    [MFLP-18]feat : 공통 응답 형태 및 에러 핸들링 구현

---
+++ meongnyang/src/main/java/com/ssafy/meongnyang/global/response/ApiResponseDto.java
@@ -0,0 +1,48 @@
+package com.ssafy.meongnyang.global.response;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.ssafy.meongnyang.global.response.enums.ErrorCode;
+import com.ssafy.meongnyang.global.response.enums.SuccessCode;
+import jakarta.annotation.Nullable;
+import lombok.Builder;
+import lombok.NonNull;
+import org.springframework.http.HttpStatus;
+
+@Builder
+public record ApiResponseDto<T>(
+        @JsonIgnore HttpStatus httpStatus,
+        int status,
+        @NonNull String message,
+        @JsonInclude(value = NON_NULL) T data
+) {
+    public static <T> ApiResponseDto<T> success(final SuccessCode successCode, @Nullable final T data) {
+        return ApiResponseDto.<T>builder()
+                .httpStatus(successCode.getHttpStatus())
+                .status(successCode.getCode())
+                .message(successCode.getMessage())
+                .data(data)
+                .build();
+    }
+
+    public static <T> ApiResponseDto<T> success(final SuccessCode successCode) {
+        return ApiResponseDto.<T>builder()
+                .httpStatus(successCode.getHttpStatus())
+                .status(successCode.getCode())
+                .message(successCode.getMessage())
+                .data(null)
+                .build();
+    }
+
+    public static <T> ApiResponseDto<T> fail(final ErrorCode errorCode) {
+        return ApiResponseDto.<T>builder()
+                .httpStatus(errorCode.getHttpStatus())
+                .status(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .data(null)
+                .build();
+    }
+}
+
---
+++ meongnyang/src/main/java/com/ssafy/meongnyang/global/response/enums/SuccessCode.java
@@ -0,0 +1,17 @@
+package com.ssafy.meongnyang.global.response.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessCode {
+
+    //200 OK
+    LOGIN_SUCCESS(200, HttpStatus.OK, "로그인 성공");
+
+    private final int code;
+    private final HttpStatus httpStatus;
+    private final String message;
+}
