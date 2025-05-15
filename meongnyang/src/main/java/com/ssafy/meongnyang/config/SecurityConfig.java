1d2e6193836a7699d8e367c16c18445459f8a622
dbscjstk3 <dbscjstk3@naver.com>
2025-05-15 17:48:35 +0900

    [MFLP-19] add : 깃 시큐리티 설정 코드 추가

---
+++ meongnyang/src/main/java/com/ssafy/meongnyang/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.ssafy.meongnyang.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    //FilterChain
+}
