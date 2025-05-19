618368653b11f1e25bf72c5e4f0e183a9f76bc75
Jayeun Park <jayeunpakr0704@gmail.com>
2025-05-19 15:35:38 +0900

    [MFLP-23] fix : 충돌 해결

--- meongnyang/src/main/java/com/ssafy/meongnyang/api/user/dto/SignUpRequest.java
+++ meongnyang/src/main/java/com/ssafy/meongnyang/api/user/dto/SignUpRequest.java
@@ -1,15 +1,25 @@
 package com.ssafy.meongnyang.api.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class SignUpRequest {
-// jakarta랑 hibernate 의존성 확인하기
+    @NotBlank(message = "이름은 필수입니다.")
     private String name;
+    @NotBlank(message = "아이디는 필수입니다.")
     private String username;
+    @NotBlank(message = "비밀번호는 필수입니다.")
     private String password;
+    @NotBlank(message = "닉네임은 필수입니다.")
     private String nickname;
+    @NotBlank(message = "전화번호는 필수입니다.")
+    @Pattern(regexp = "^(010)[0-9]{7,8}$", message = "유효한 전화번호 형식이어야 합니다. (예: 01012345678)")
     private String phonenumber;
+    @NotBlank(message="이메일은 필수입니다.")
+    @Email(message="이메일 형식이 유효하지 않습니다.")
     private String email;
 
     public void setName(String name) {
