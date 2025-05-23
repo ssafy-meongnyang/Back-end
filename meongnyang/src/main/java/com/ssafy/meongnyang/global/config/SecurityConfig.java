f7adbd5c5bd52e8a52d5afc9d74eb6fd5e9a8144
Jayeun Park <jayeunpakr0704@gmail.com>
2025-05-23 09:30:25 +0900

    [MFLP-37] feat : Vue - SpringBoot 연결을 위한 SecurityConfig 설정 구현

--- meongnyang/src/main/java/com/ssafy/meongnyang/global/config/SecurityConfig.java
+++ meongnyang/src/main/java/com/ssafy/meongnyang/global/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,6 +14,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -35,6 +41,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults()) // CORS 설정 허용 단순히 CorsConfigurationSource 빈만 적용
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session
@@ -54,6 +61,20 @@ public class SecurityConfig {
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+    // Spring Security가 활성화 된 경우 CORS 요청이 먼저 Spring Security 필터체인을 거치기 때문에 빈 등록 필요
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(List.of("http://localhost:*"));
+        config.setAllowCredentials(true); // 쿠키 허용 여부
+        config.addAllowedHeader("*");     // 요청 헤더 허용
+        config.addAllowedMethod("*");     // 요청 메서드 허용
+        config.setExposedHeaders(List.of("Authorization")); // 클라이언트에서 Authorization 헤더 접근 가능
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
 }
